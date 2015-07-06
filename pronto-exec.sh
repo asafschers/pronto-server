e961045b0b25234003dbabe4bec15ddaf72276b6
asaf schers <schers@riskified.com>
2015-07-06 15:08:50 +0300

    remove github access token

--- pronto-exec.sh
+++ pronto-exec.sh
@@ -2,5 +2,6 @@
 pushd /Users/asafschers1/Development/riskified/shopify_app
 source ~/.rvm/scripts/rvm
 type rvm | head -n 1
-GITHUB_ACCESS_TOKEN=2ec317dff980ebb5b1f619b330455b2845c5e089 PULL_REQUEST_ID=958 pronto run -f github_pr -c origin/master
+# set GITHUB_ACCESS_TOKEN
+PULL_REQUEST_ID=958 pronto run -f github_pr -c origin/master
 popd
